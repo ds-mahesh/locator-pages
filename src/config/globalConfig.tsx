a00e4e9c98cb39714934d2d757bc17ccb5042d7d
ds-mahesh <mahesh.chand@dotsquare.com>
2023-01-25 14:31:39 +0530

    locator commit

---
+++ src/config/globalConfig.tsx
@@ -0,0 +1,39 @@
+export const limit = 3; 
+
+export const radius = 50;
+
+export const defaultQuery = "";
+
+export const baseApiUrl = "https://liveapi-sandbox.yext.com/v2/accounts/me";
+
+export const liveAPIKey = "516ca7869d70e6b97458f4c534ed4983";
+
+export const savedFilterId = "";
+
+export const entityTypes = "location";
+
+export const googleMapsConfig =  {
+    centerLatitude:	51.5095146286,
+    centerLongitude:-0.1244828354,
+    googleMapsApiKey: "AIzaSyDZNQlSlEIkFAct5VzUtsP4dSbvOr2bE18"   
+};
+
+export const AnswerExperienceConfig =  {
+    experienceKey: "new-location",
+    locale:"en",
+    apiKey: "bd7dc040fd7cca80bca7905845092d43",
+    verticalKey: "locations",
+    experienceVersion: "STAGING",
+    sessionTrackingEnabled: true,
+    endpoints:{
+        universalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/query",
+        verticalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/query",
+        questionSubmission: "https://liveapi-sandbox.yext.com/v2/accounts/me/createQuestion",
+        universalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete",
+        verticalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete",
+        filterSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/filtersearch"
+    }
+};
+
+
+
